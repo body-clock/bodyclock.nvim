69c0977dad76635f00b86fc3c16b5ce0f796cc3f
Patrick Perkins <pperkins@upenn.edu>
2025-02-17 13:51:25 -0500

    format with conform

---
+++ lua/config/plugins/format.lua
@@ -0,0 +1,12 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			ruby = { "rubocop" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+	},
+}
