9f1d4ef935ca80808f653b29fe624431ef1364a3
Patrick Perkins <pperkins@upenn.edu>
2025-02-20 21:07:48 -0500

    add some purple to gruvbox telescope

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,9 +1,24 @@
+---@diagnostic disable: missing-fields
 return {
 	{
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
 		opts = ...,
 		config = function()
+			require("gruvbox").setup({
+				overrides = {
+					-- Overriding some default Telescope colors to avoid boring white borders, yay purple!
+					-- TelescopeNormal = { link = "GruvboxFg1" },
+					TelescopeSelection = { link = "GruvboxPurple" }, -- Default GruvboxOrangeBold
+					-- TelescopeSelectionCaret = { link = "GruvboxRed" },
+					-- TelescopeMultiSelection = { link = "GruvboxGray" },
+					TelescopeBorder = { link = "GruvboxPurple" }, -- default TelescopeNormal
+					TelescopePromptBorder = { link = "GruvboxPurple" }, -- default TelescopeNormal
+					TelescopeResultsBorder = { link = "GruvboxPurple" }, -- default TelescopeNormal
+					TelescopePreviewBorder = { link = "GruvboxPurple" }, -- default TelescopeNormal
+					-- TelescopeMatching = { link = "GruvboxBlue" }, -- default GruvboxBlue
+				},
+			})
 			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
