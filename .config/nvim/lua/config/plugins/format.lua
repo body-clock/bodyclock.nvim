445f93e68c1c5dfe18ba0a98946e02a24ba83336
Patrick Perkins <pperkins@upenn.edu>
2025-04-03 16:25:57 -0400

    use erb_lint for formatting, backup to erb_formatter

--- .config/nvim/lua/config/plugins/format.lua
+++ .config/nvim/lua/config/plugins/format.lua
@@ -3,11 +3,20 @@ return {
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	opts = {
+		formatters = {
+			erb_lint = {
+				stdin = false,
+				tmpfile_format = ".conform.$RANDOM.$FILENAME",
+				command = "erb_lint",
+				args = { "--autocorrect", "$FILENAME" },
+			},
+		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			ruby = { "rubocop" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
-			eruby = { "erb_format" }
+			-- requires a local .erb_lint.yml config file
+			eruby = { "erb_lint", "erb_format", stop_after_first = true },
 		},
 	},
 }
