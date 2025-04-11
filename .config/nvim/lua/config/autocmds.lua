080990c4a560cd720311803e069cb7551639b99f
Patrick Perkins <pperkins@upenn.edu>
2025-04-11 12:11:32 -0400

    disable command line window activation with q:

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,25 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- Overengineered solution to accidentally opening command line history
+local function escape(keys)
+	return vim.api.nvim_replace_termcodes(keys, true, false, true)
+end
+
+vim.keymap.set("c", "<C-f>", function()
+	vim.g.requested_cmdwin = true
+	vim.api.nvim_feedkeys(escape("<C-f>"), "n", false)
+end)
+
+vim.api.nvim_create_autocmd("CmdWinEnter", {
+	group = vim.api.nvim_create_augroup("CWE", { clear = true }),
+	pattern = "*",
+	callback = function()
+		if vim.g.requested_cmdwin then
+			vim.g.requested_cmdwin = nil
+		else
+			vim.api.nvim_feedkeys(escape(":q<CR>:"), "m", false)
+		end
+	end,
+})
