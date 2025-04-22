7e3a3deb73c87d03ae12a74086f3acb2f6894677
Patrick Perkins <pperkins@upenn.edu>
2025-04-22 10:33:45 -0400

    try switching to tinted-nvim for auto tinty integration, switch colors for rainbow brackets

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -2,6 +2,7 @@
 return {
 	{
 		"tinted-theming/tinted-vim",
+		enabled = false,
 		config = function()
 			local default_theme = "base16-tarot"
 
@@ -37,6 +38,31 @@ return {
 			end
 
 			main()
+		end,
+	},
+	-- TODO: I'm trying this `tinted-nvim` alternative to the above
+	-- which is supposed to work more fluidly than `tinted-vim` and
+	-- has some nice reloading functionality and tinty integration.
+	-- It seems to be working fine for now, but sometimes the gitsigns
+	-- or diff doesn't color properly. If that continues to happen, I'll
+	-- go back to the above.
+	{
+		"tinted-theming/tinted-nvim",
+		dependencies = {
+			{ "rktjmp/fwatch.nvim" },
+		},
+		config = function()
+			vim.o.termguicolors = true
+			vim.g.tinted_colorspace = 256
+			local tinted = require("tinted-colorscheme")
+			tinted.setup(nil, {
+				supports = {
+					live_reload = true,
+				},
+				highlights = {
+					telescope_borders = true,
+				},
+			})
 		end,
 	},
 }
--- .config/nvim/lua/config/plugins/rainbow-delimiters.lua
+++ .config/nvim/lua/config/plugins/rainbow-delimiters.lua
@@ -4,13 +4,13 @@ return {
 		require("rainbow-delimiters.setup").setup({
 			highlight = {
 				-- tinted_colors:
-				"tinted_gui08", -- bright red
-				"tinted_gui0A", -- bright yellow
-				"tinted_gui0D", -- bright blue
-				"tinted_gui09", -- orange
-				"tinted_gui0B", -- bright green
-				"tinted_gui0E", -- bright purple
-				"tinted_gui0C", -- bright cyan
+				"rainbowcol1",
+				"rainbowcol2",
+				"rainbowcol3",
+				"rainbowcol4",
+				"rainbowcol5",
+				"rainbowcol6",
+				"rainbowcol7",
 			},
 		})
 	end,
