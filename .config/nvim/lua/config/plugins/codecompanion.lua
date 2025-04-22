79deca21f787cce06eaf21548a049b12628c0ddb
Patrick Perkins <pperkins@upenn.edu>
2025-04-22 10:29:00 -0400

    add custom prompt for codecompanion commit messages

--- .config/nvim/lua/config/plugins/codecompanion.lua
+++ .config/nvim/lua/config/plugins/codecompanion.lua
@@ -15,5 +15,37 @@ return {
 				})
 			end,
 		},
+		prompt_library = {
+			["Generate a Commit Message"] = {
+				strategy = "chat",
+				description = "Generate a commit message",
+				opts = {
+					index = 10,
+					is_default = true,
+					is_slash_cmd = true,
+					short_name = "commit",
+					auto_submit = true,
+				},
+				prompts = {
+					{
+						role = "user",
+						content = function()
+							return string.format(
+								[[You are an expert at generating concise commit messages. They should be one or two sentences, but descriptive enough that they are informative. Given the git diff listed below, please generate a commit message for me:
+
+```diff
+%s
+```
+]],
+								vim.fn.system("git diff --no-ext-diff --staged")
+							)
+						end,
+						opts = {
+							contains_code = true,
+						},
+					},
+				},
+			},
+		},
 	},
 }
