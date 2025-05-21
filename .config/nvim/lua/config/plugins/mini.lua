2610526826c54a6ea6be1d57080972b31d96293b
Patrick Perkins <pperkins@upenn.edu>
2025-05-21 12:41:17 -0400

    alphabetize, clean up mini config

--- .config/nvim/lua/config/plugins/mini.lua
+++ .config/nvim/lua/config/plugins/mini.lua
@@ -2,29 +2,30 @@ return {
 	{
 		"echasnovski/mini.nvim",
 		config = function()
-			require("mini.ai").setup({})
-			require("mini.surround").setup({})
-			require("mini.pairs").setup({})
-			require("mini.sessions").setup({
-				autoread = true,
-				autowrite = true,
-			})
-			require("mini.indentscope").setup({
-				draw = {
-					delay = 0,
-					animation = require("mini.indentscope").gen_animation.none(),
-				},
-			})
-			require("mini.notify").setup({})
-			require("mini.icons").setup({})
-			require("mini.statusline").setup({})
-
+			require("mini.ai").setup()
 			local hipatterns = require("mini.hipatterns")
 			hipatterns.setup({
 				highlighters = {
 					hex_color = hipatterns.gen_highlighter.hex_color(),
 				},
 			})
+			require("mini.icons").setup()
+			require("mini.indentscope").setup({
+				draw = {
+					delay = 0,
+					animation = require("mini.indentscope").gen_animation.none(),
+				},
+			})
+			local notify = require("mini.notify")
+			notify.setup()
+			vim.notify = notify.make_notify()
+			require("mini.pairs").setup()
+			require("mini.sessions").setup({
+				autoload = true,
+				autosave = true,
+			})
+			require("mini.statusline").setup()
+			require("mini.surround").setup()
 		end,
 	},
 }
